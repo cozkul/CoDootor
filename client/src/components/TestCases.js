a9ba1d0f1eaace3acd838c4faccf882e2643b7bc
Cinar Ozkul <22607011+cozkul@users.noreply.github.com>
2024-07-01 12:53:34 -0700

    Add UI for test result display

--- client/src/components/TestCases.js
+++ client/src/components/TestCases.js
@@ -1,5 +1,5 @@
 
-import { Tabs, rem } from '@mantine/core';
+import { Center, Paper, Rating, Text, Stack, Tabs, rem } from '@mantine/core';
 import { IconCircleDotted, IconCheck, IconX } from '@tabler/icons-react';
 
 
@@ -8,31 +8,51 @@ const TestCases = () => {
   const iconStyle = { width: rem(12), height: rem(12) };
 
   return (
-    <Tabs defaultValue="test1">
-      <Tabs.List>
-        <Tabs.Tab value="test1" leftSection={<IconCircleDotted style={iconStyle} />}>
-          Test 1
-        </Tabs.Tab>
-        <Tabs.Tab value="test2" leftSection={<IconCheck style={iconStyle} />}>
-          Test 2
-        </Tabs.Tab>
-        <Tabs.Tab value="test3" leftSection={<IconX style={iconStyle} />}>
-          Test 3
-        </Tabs.Tab>
-      </Tabs.List>
+    <Stack
+      h={300}
+      bg="var(--mantine-color-body)"
+      align="stretch"
+      justify="top"
+      gap="md"
+    >
+      
+        <Tabs defaultValue="test1">
+          <Tabs.List>
+            <Tabs.Tab value="test1" leftSection={<IconCircleDotted style={iconStyle} />}>
+              Test 1
+            </Tabs.Tab>
+            <Tabs.Tab value="test2" leftSection={<IconCheck style={iconStyle} />}>
+              Test 2
+            </Tabs.Tab>
+            <Tabs.Tab value="test3" leftSection={<IconX style={iconStyle} />}>
+              Test 3
+            </Tabs.Tab>
+          </Tabs.List>
 
-      <Tabs.Panel value="test1">
-        Test 1 tab content
-      </Tabs.Panel>
+          <Tabs.Panel value="test1">
+            Test 1 tab content
+          </Tabs.Panel>
 
-      <Tabs.Panel value="test2">
-        Test 2 tab content
-      </Tabs.Panel>
+          <Tabs.Panel value="test2">
+            Test 2 tab content
+          </Tabs.Panel>
 
-      <Tabs.Panel value="test3">
-        Test 3 tab content
-      </Tabs.Panel>
-  </Tabs>
+          <Tabs.Panel value="test3">
+            Test 3 tab content
+          </Tabs.Panel>
+      </Tabs>
+      <Paper shadow="xs" withBorder p="xl">
+        <Stack
+          bg="var(--mantine-color-body)"
+          align="stretch"
+          justify="center"
+          gap="md"
+        >
+          <Center><Text>3/5 tests passed</Text></Center>
+          <Center><Rating count={3} defaultValue={2} readOnly/></Center>
+        </Stack>
+      </Paper>
+    </Stack>
   );
 };
 
