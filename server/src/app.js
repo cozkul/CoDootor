5b421a8bb5e3e4cf787e22feee38c784720e004f
deraphel <75043986+jho1211@users.noreply.github.com>
2024-07-03 23:46:07 -0700

    Combined grade and generate code endpoints
    Still kept the code endpoint though

--- server/src/app.js
+++ server/src/app.js
@@ -54,12 +54,32 @@ app.post('/code', async (req, res) => {
 /*
     API endpoint to submit generated code for scoring via
     running the unit tests against the code
+
+    Test manually using:
+    curl -d "{\"desc\": \"Takes in two numbers and returns the sum of both of them\", \"id\": 1}" \
+    --header "Content-Type: application/json" localhost:5001/grade
 */
-app.post('/grade', (req, res) => {
-  var testResults;
-  if (req.body) {
-    testResults = oa.TestGeneratedCode(req.body);
+app.post('/grade', async (req, res) => {
+  const desc = req.body.desc;
+  const id = req.body.id;
+  
+  // If any of the required parameters are missing send a 400 response
+  if (desc == null || desc == "") return res.status(400).json({"error": "No description was provided."});
+  if (id == null || id == "") return res.status(400).json({"error": "No question ID was provided."});
+  
+  // If description is malicious, then send 400 response
+  if (oa.isMalicious(desc)) {
+    return res.status(400).json({"error": "Malicious description included, modify your description."});
   }
+  
+  // Get the LLM code as an object based on the given description
+  var resp = await oa.FetchResponse(desc);
+
+  // If valid response, then append the question ID and submit for grading
+  if (resp) resp.id = id;
+  else return res.status(400).json({"error": "Failed to generate code from Ollama"});
+
+  var testResults = oa.TestGeneratedCode(resp);
   
   if (testResults && testResults.length >= 1) {
     if (testResults[0].err) res.status(400).json(testResults);
