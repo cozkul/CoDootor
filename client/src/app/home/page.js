61b8a4941cb1e6df9e0819fb9bd32a201200b3d3
cgj-tang <67113323+cgj-tang@users.noreply.github.com>
2024-07-13 23:48:07 -0700

    landing page, moved home page

---
+++ client/src/app/home/page.js
@@ -0,0 +1,49 @@
+'use server'
+
+import styles from "./page.module.css";
+import { Space, Image, Title } from '@mantine/core';
+import { NavbarSimple } from "@/components/NavbarSimple/NavbarSimple";
+import QuestionList from "@/components/QuestionList";
+import { getSession } from '@auth0/nextjs-auth0';
+import LoginPrompt from "@/components/LoginPrompt";
+import UserBanner from "@/components/UserBanner";
+import { getAccessToken } from '@auth0/nextjs-auth0';
+
+export default async function Home() {
+  const sessionInfo = await getSession();
+
+  if (sessionInfo == null) return (
+  <div className={styles.loginPromptPage}>
+    <LoginPrompt></LoginPrompt>
+  </div>
+  )
+
+  const { accessToken } = await getAccessToken();
+  const questions = await fetch(`http://host.docker.internal:5001/question_list`, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`
+    }
+  }
+  )
+    .then(res => res.json())
+    .then(res => JSON.parse(res))
+    .then(res => res.question_list)
+    .catch(error => console.error('Error fetching data:', error));
+
+  return (
+    <div>
+      <div className={styles.page}>
+        <div>
+          <NavbarSimple />
+        </div>
+        <div className={styles.centerColumn}>
+          <UserBanner sessionInfo={sessionInfo}/>
+          <br></br>
+          <QuestionList questions={questions} />
+        </div>
+        
+
+      </div>
+    </div>
+  )
+}
