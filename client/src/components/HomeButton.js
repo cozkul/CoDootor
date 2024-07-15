0ba4ff454ff7badc6aca17edf187a61d1241ddfa
Leo Kim <81009412+leokim0922@users.noreply.github.com>
2024-07-14 20:36:55 -0700

    Updated HomeButton copmonent to display updated user score every time

--- client/src/components/HomeButton.js
+++ client/src/components/HomeButton.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Link from 'next/link';
+//import Link from 'next/link';
 import { Button } from '@mantine/core'
 
 // link to the home page
 const HomeButton = () => {
+  const handleHomeClick = () => {
+    window.location.href = "/home";
+  };
+
   return (
-    <Link href="/home">
-      <Button variant="filled">Home</Button>
-    </Link>
+    <Button variant="filled" onClick={handleHomeClick}>
+      Home
+    </Button>
   );
 };
 
