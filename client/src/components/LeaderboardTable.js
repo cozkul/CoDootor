a44d76705925890fb88361097ee57f160afe0685
deraphel <75043986+jho1211@users.noreply.github.com>
2024-07-14 22:07:30 -0700

    implement leaderboard

--- client/src/components/LeaderboardTable.js
+++ client/src/components/LeaderboardTable.js
@@ -12,7 +12,7 @@ import QuestionButton from "@/components/QuestionButton";
 export default function LeaderboardTable({ users, cur_user_id }) {
     const ranks = users.map((user, index) => ({
         id: user.user_id,
-        rank: index,
+        rank: index + 1,
         nick: user.nickname,
         points: user.num_points
     }));
