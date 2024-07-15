86b749f3a9dde49b8d07a31ba1ed6a933b6c1ac2
deraphel <75043986+jho1211@users.noreply.github.com>
2024-07-14 17:04:37 -0700

    Completed user data functions

--- server/src/user_data.js
+++ server/src/user_data.js
@@ -7,6 +7,21 @@ function getUsers() {
     return users;
 }
 
+/*
+    Gets the data for the specified user from file
+*/
+function getUserDataFile(folder, userID) {
+    if (!userID) return null;
+    const baseUserPath = path.join(__dirname, '..', `/${folder}/`);
+
+    try {
+        const user = JSON.parse(fs.readFileSync(baseUserPath + userID + ".json"));
+        return user;
+    } catch (e) {
+        return null;
+    }
+}
+
 // Reads all the user data files stored in folder and stores it in users
 function loadUserDataOnStart(folder) {
     users = []
@@ -28,7 +43,7 @@ function loadUserDataOnStart(folder) {
 }
 
 // Initialize new user data for user with given userID and nickname
-function initializeUserData(folder, userID, nickname) {
+function initializeUserDataFile(folder, userID, nickname) {
     const baseUserPath = path.join(__dirname, '..', `/${folder}/`);
     var files;
 
@@ -58,12 +73,79 @@ function initializeUserData(folder, userID, nickname) {
     return "success";
 }
 
-/*
-    Given the user's ID and the score data for a question (questionID, score)
-    update the user's score for that question in the database
+/* 
+    Overwrites the current user data file with the new one, assuming it already exists.
+    Otherwise, it will create a new file for the user.
 */
-function updateQuestionDataForUser(userID, questionData) {
+function updateUserDataFile(folder, user) {
+    if (!folder || !user) return null;
 
+    const baseUserPath = path.join(__dirname, '..', `/${folder}/`);
+    try {
+        fs.writeFileSync(baseUserPath + user.user_id + ".json", JSON.stringify(user));
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
+
+    return "success";
 }
 
-module.exports = {loadUserDataOnStart, initializeUserData, updateQuestionDataForUser, getUsers};
+/*
+    Finds the relevant question based on qid for a user and updates their score with
+    the max of the curScore and newScore
+
+    Returns the updated user object
+*/
+function updateQuestionScore(user, questionData) {
+    if (!user || !questionData) return null;
+
+    const qs = Object.keys(user.questions_solved);
+
+    for (var i = 0; i < qs.length; i++) {
+        // If the questionID already exists and they scored higher, then increase points and score
+        // otherwise, if it exists, do nothing
+        if ((qs[i] == questionData.qid)) { 
+            if (questionData.score > user.questions_solved[qs[i]]) {
+                user.num_points += questionData.score - user.questions_solved[qs[i]];
+                user.questions_solved[qs[i]] = questionData.score
+            }
+            
+            return user;
+        }
+    }
+
+    // If it doesn't exist, then increase their points and question score
+    user.questions_solved[questionData.qid] = questionData.score;
+    user.num_points += questionData.score;
+    return user;
+}
+
+/*
+    Given the user's ID and the score data for a question (qid, score)
+    update the user's score for that question in the database
+
+    Assumes that the given question data (qid, score) are valid
+*/
+function updatedUserFileWithNewScore(folder, userID, questionData) {
+    // If missing userID or questionData, then return null;
+    if (!userID || !questionData) return null;
+
+    // Find the user in the data folder
+    const user = getUserDataFile(folder, userID);
+    if (!user) return null;
+
+    // Update the question score if user exists
+    const updatedUser = updateQuestionScore(user, questionData);
+
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].user_id == updatedUser.user_id) users[i] = updatedUser;
+    }
+
+    const res = updateUserDataFile(folder, updatedUser);
+
+    if (res) return "success"
+    else return null;
+}
+
+module.exports = {getUserDataFile, loadUserDataOnStart, initializeUserDataFile, updateUserDataFile, updateQuestionScore, updatedUserFileWithNewScore, getUsers};
--- server/test/user_data.test.js
+++ server/test/user_data.test.js
@@ -5,13 +5,14 @@ const path = require('path');
 
 const user0 = {"user_id": "1234567890", "nickname": "mikey"};
 const user1 = {"user_id": "2345678901", "nickname": "davey"};
+const testFolderName = "testDataFolder";
 
 const clearFolder = () => {
-    const fp = path.join(__dirname, "..", "/testDataFolder")
+    const fp = path.join(__dirname, "../", testFolderName)
     try {
         fs.rmSync(fp, {recursive: true});
         fs.mkdirSync(fp);
-        udata.loadUserDataOnStart("testDataFolder");
+        udata.loadUserDataOnStart(testFolderName);
     } catch (e) {
     }
 }
@@ -21,16 +22,20 @@ describe("Tests for user_data functions", function () {
         clearFolder();
     })
 
+    after(function () {
+        clearFolder();
+    })
+
     describe("Testing the loadUserDataOnStart function", function () {
         it('Testing an empty folder', function () {
-            const users = udata.loadUserDataOnStart("testDataFolder");
+            const users = udata.loadUserDataOnStart(testFolderName);
             expect(users.length).to.equal(0);
         });
 
         it('Testing a non-empty folder', function () {
-            udata.initializeUserData("testDataFolder", user0.user_id, user0.nickname);
-            udata.initializeUserData("testDataFolder", user1.user_id, user1.nickname);
-            const users = udata.loadUserDataOnStart("testDataFolder");
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            udata.initializeUserDataFile(testFolderName, user1.user_id, user1.nickname);
+            const users = udata.loadUserDataOnStart(testFolderName);
             expect(users.length).to.equal(2);
         });
 
@@ -48,56 +53,247 @@ describe("Tests for user_data functions", function () {
         });
     })
 
-    describe("Testing the initializeUserData function", function () {
+    describe("Testing the initializeUserDataFile function", function () {
         it('Initializing an existing user', function () {
-            let res = udata.initializeUserData("testDataFolder", user0.user_id, user0.nickname);
-            console.log(res);
+            let res = udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
             expect(udata.getUsers().length).to.equal(1);
             expect(res).to.equal("success");
 
-            res = udata.initializeUserData("testDataFolder", user0.user_id, user0.nickname);
+            res = udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
             expect(udata.getUsers().length).to.equal(1);
             expect(res).to.equal(null);
         });
 
         it('Initializing multiple users', function () {
-            let res = udata.initializeUserData("testDataFolder", user0.user_id, user0.nickname);
+            let res = udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
             expect(udata.getUsers().length).to.equal(1);
             expect(res).to.equal("success");
 
-            res = udata.initializeUserData("testDataFolder", user1.user_id, user1.nickname);
+            res = udata.initializeUserDataFile(testFolderName, user1.user_id, user1.nickname);
             expect(udata.getUsers().length).to.equal(2);
             expect(res).to.equal("success");
         });
 
         it('Initializing with null and invalid folder', function () {
-            let res = udata.initializeUserData(null, user0.user_id, user0.nickname);
+            let res = udata.initializeUserDataFile(null, user0.user_id, user0.nickname);
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
 
-            res = udata.initializeUserData("nonExistentFolder", user0.user_id, user0.nickname);
+            res = udata.initializeUserDataFile("nonExistentFolder", user0.user_id, user0.nickname);
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
         });
 
         it('Initializing with null/missing userid/nickname', function () {
-            let res = udata.initializeUserData("testDataFolder", null, null);
+            let res = udata.initializeUserDataFile(testFolderName, null, null);
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
 
-            res = udata.initializeUserData("nonExistentFolder");
+            res = udata.initializeUserDataFile("nonExistentFolder");
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
         });
 
         it('Initializing with all null/missing params', function () {
-            let res = udata.initializeUserData(null, null, null);
+            let res = udata.initializeUserDataFile(null, null, null);
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
 
-            res = udata.initializeUserData();
+            res = udata.initializeUserDataFile();
             expect(res).to.equal(null);
             expect(udata.getUsers().length).to.equal(0);
         });
     })
+    
+    describe("Testing the updateUserDataFile function", function () {
+        it('Updating an existing user, correct directory', function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            const updatedUser = {
+                "user_id": "1234567890",
+                "nickname": "mikey",
+                "questions_solved": {
+                    "1": 3,
+                    "2": 2
+                },
+                "num_points": 5
+            }
+
+            res = udata.updateUserDataFile(testFolderName, updatedUser);
+            expect(res).to.equal("success");
+
+            const retrievedUser = udata.getUserDataFile(testFolderName, updatedUser.user_id);
+            expect(retrievedUser.user_id).to.equal(updatedUser.user_id);
+            expect(retrievedUser.nickname).to.equal(updatedUser.nickname);
+            expect(retrievedUser.questions_solved["1"]).to.equal(3);
+            expect(retrievedUser.questions_solved["2"]).to.equal(2);
+            expect(retrievedUser.num_points).to.equal(5);
+        });
+
+        it('Updating an existing user, wrong directory', function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            const updatedUser = {
+                "user_id": "1234567890",
+                "nickname": "mikey",
+                "questions_solved": {
+                    "1": 3,
+                    "2": 2
+                },
+                "num_points": 5
+            }
+            res = udata.updateUserDataFile("nonExistentFolder", updatedUser);
+            expect(res).to.equal(null);
+        });
+
+        it('Updating with null/undefined params', function () {
+            expect(udata.updateUserDataFile(null, null)).to.equal(null);
+            expect(udata.updateUserDataFile()).to.equal(null);
+            expect(udata.updateUserDataFile(null, user0)).to.equal(null);
+            expect(udata.updateUserDataFile(testFolderName, null)).to.equal(null);
+        });
+
+        it('Updating non-existent user, correct directory', function () {
+            const updatedUser = {
+                "user_id": "1234567890",
+                "nickname": "mikey",
+                "questions_solved": {
+                    "1": 3,
+                    "2": 2
+                },
+                "num_points": 5
+            }
+            res = udata.updateUserDataFile(testFolderName, updatedUser);
+            expect(res).to.equal("success");
+
+            const retrievedUser = udata.getUserDataFile(testFolderName, updatedUser.user_id);
+            expect(retrievedUser.user_id).to.equal(updatedUser.user_id);
+            expect(retrievedUser.nickname).to.equal(updatedUser.nickname);
+            expect(retrievedUser.questions_solved["1"]).to.equal(3);
+            expect(retrievedUser.questions_solved["2"]).to.equal(2);
+            expect(retrievedUser.num_points).to.equal(5);
+        });
+    })
+
+    describe("Testing the updateQuestionScore function", function () {
+        const newQuestionData = {
+            "qid": "2",
+            "score": 2
+        }
+        const newQuestionDataHigher = {
+            "qid": "2",
+            "score": 3
+        }
+        const newQuestionDataLower = {
+            "qid": "2",
+            "score": 1
+        }
+
+        it("Testing null/undefined params", function () {
+            expect(udata.updateQuestionScore(null, null)).to.equal(null);
+            expect(udata.updateQuestionScore()).to.equal(null);
+            expect(udata.updateQuestionScore(null, newQuestionData)).to.equal(null);
+            expect(udata.updateQuestionScore(user0, null)).to.equal(null);
+        })
+
+        it("Testing valid user, new score", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+
+            let updatedUser = udata.updateQuestionScore(user, newQuestionData)
+            expect(updatedUser.questions_solved[newQuestionData.qid]).to.equal(newQuestionData.score);
+            expect(updatedUser.num_points).to.equal(2);
+        })
+
+        it("Testing valid user, lower score", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+
+            let updatedUser0 = udata.updateQuestionScore(user, newQuestionData);
+            let updatedUser1 = udata.updateQuestionScore(updatedUser0, newQuestionDataLower);
+
+            expect(updatedUser1.questions_solved[newQuestionData.qid]).to.equal(newQuestionData.score);
+            expect(updatedUser1.num_points).to.equal(2);
+        })
+
+        it("Testing valid user, higher score", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+
+            let updatedUser0 = udata.updateQuestionScore(user, newQuestionData);
+            let updatedUser1 = udata.updateQuestionScore(updatedUser0, newQuestionDataHigher);
+            expect(updatedUser1.questions_solved[newQuestionData.qid]).to.equal(newQuestionDataHigher.score);
+            expect(updatedUser1.num_points).to.equal(3);
+        })
+    });
+
+    describe("Testing the updatedUserFileWithNewScore function", function () {
+        const newQuestionData = {
+            "qid": "2",
+            "score": 2
+        }
+        const newQuestionDataHigher = {
+            "qid": "2",
+            "score": 3
+        }
+        const newQuestionDataLower = {
+            "qid": "2",
+            "score": 1
+        }
+
+        const diffQuestionData = {
+            "qid": "3",
+            "score": 3
+        }
+
+        it("Testing null/undefined params", function () {
+            expect(udata.updatedUserFileWithNewScore(testFolderName, null, null)).to.equal(null);
+            expect(udata.updatedUserFileWithNewScore("nonExistentFolder", null, null)).to.equal(null);
+            expect(udata.updatedUserFileWithNewScore(null, null, null)).to.equal(null);
+            expect(udata.updatedUserFileWithNewScore(null, user0.user_id, null)).to.equal(null);
+            expect(udata.updatedUserFileWithNewScore(null, null, user0.user_id)).to.equal(null);
+            expect(udata.updatedUserFileWithNewScore()).to.equal(null);
+        });
+
+        it("Testing updating the score for a new question", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            let res = udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionData);
+            expect(res).to.equal("success");
+
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+            expect(user.questions_solved[newQuestionData.qid]).to.equal(newQuestionData.score);
+            expect(user.num_points).to.equal(2);
+        });
+
+        it("Testing updating the score for an existing question with lower score", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionData);
+            let res = udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionDataLower);
+            expect(res).to.equal("success");
+
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+            expect(user.questions_solved[newQuestionData.qid]).to.equal(newQuestionData.score);
+            expect(user.num_points).to.equal(2);
+        });
+
+        it("Testing updating the score for an existing question with higher score", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionData);
+            let res = udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionDataHigher);
+            expect(res).to.equal("success");
+
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+            expect(user.questions_solved[newQuestionData.qid]).to.equal(newQuestionDataHigher.score);
+            expect(user.num_points).to.equal(3);
+        });
+
+        it("Testing updating the score for multiple questions", function () {
+            udata.initializeUserDataFile(testFolderName, user0.user_id, user0.nickname);
+            udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, newQuestionData);
+            udata.updatedUserFileWithNewScore(testFolderName, user0.user_id, diffQuestionData);
+
+            let user = udata.getUserDataFile(testFolderName, user0.user_id);
+            expect(user.questions_solved[newQuestionData.qid]).to.equal(newQuestionData.score);
+            expect(user.questions_solved[diffQuestionData.qid]).to.equal(diffQuestionData.score);
+            expect(user.num_points).to.equal(5);
+        });
+    });
 });
