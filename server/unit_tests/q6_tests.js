4417e75cc6fad30233642c24c85b3a9e4afa885f
Leo Kim <81009412+leokim0922@users.noreply.github.com>
2024-07-26 01:17:20 -0700

    Updated Q6,7 to new format

--- server/unit_tests/q6_tests.js
+++ server/unit_tests/q6_tests.js
@@ -1,57 +1,126 @@
-function test0(fn) {
-    var pts = 0;
-
-    if (fn("") === true) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "Test with empty string", score: pts};
-}
-
-function test1(fn) {
-    var pts = 0;
-
-    if (fn("a") === true) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "A test with a single character", score: pts};
-}
-
-function test2(fn) {
-    var pts = 0;
-
-    if (fn("racecar") == true) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "Test with input word \"racecar\".", score: pts};
-}
-
-function test3(fn) {
-    var pts = 0;
-
-    if (fn("mlem mlom") == false) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "A secret test with a mystery character sequence.", score: pts};
-}
-
-function test4(fn) {
-    var pts = 0;
-
-    if (fn("The quick brown fox jumps over the lazy dog.") == false) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "Test with input sentence \"The quick brown fox jumps over the lazy dog.\".", score: pts};
+var tests = {
+    "test1": {
+        "input_args": [[""]],
+        "expected_outputs": [true],
+        "pts": 1,
+        "desc": "Test with empty string.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test2": {
+        "input_args": [["a"]],
+        "expected_outputs": [true],
+        "pts": 1,
+        "desc": "A test with a single character.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test3": {
+        "input_args": [["racecar"]],
+        "expected_outputs": [true],
+        "pts": 1,
+        "desc": "Test with input word 'racecar'.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test4": {
+        "input_args": [["mlem mlom"]],
+        "expected_outputs": [false],
+        "pts": 1,
+        "desc": "A secret test with a mystery character sequence.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test5": {
+        "input_args": [["The quick brown fox jumps over the lazy dog."]],
+        "expected_outputs": [false],
+        "pts": 1,
+        "desc": "Test with input sentence 'The quick brown fox jumps over the lazy dog.'.",
+        "passed": false,
+        "actual_outputs": []
+    }
 }
 
 function run_tests(fn) {
-    var tests = [test0, test1, test2, test3, test4];
-    var results = [];
+    var results = []
+    const keys = Object.keys(tests);
+    for (var i = 0; i < keys.length; i++) {
+        const curTest = tests[keys[i]]
+        const args = curTest.input_args;
+        const outputs = curTest.expected_outputs;
+        results.push(curTest);
+        for (var j = 0; j < curTest.input_args.length; j++) {
+            try {
+                const actualOutput = fn(...args[j])
+                const expectedOutput = outputs[j]
 
-    for (var i = 0; i < tests.length; i++) {
-        results.push(tests[i](fn));
+                if (actualOutput == expectedOutput) {
+                    results[i].passed = true;
+                    results[i].actual_outputs.push(actualOutput);
+                }
+            } catch (err) {
+                results[i].actual_outputs.push(err);
+            }
+        }
     }
 
     return results;
 }
+
+// function test0(fn) {
+//     var pts = 0;
+
+//     if (fn("") === true) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "Test with empty string", score: pts};
+// }
+
+// function test1(fn) {
+//     var pts = 0;
+
+//     if (fn("a") === true) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "A test with a single character", score: pts};
+// }
+
+// function test2(fn) {
+//     var pts = 0;
+
+//     if (fn("racecar") == true) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "Test with input word \"racecar\".", score: pts};
+// }
+
+// function test3(fn) {
+//     var pts = 0;
+
+//     if (fn("mlem mlom") == false) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "A secret test with a mystery character sequence.", score: pts};
+// }
+
+// function test4(fn) {
+//     var pts = 0;
+
+//     if (fn("The quick brown fox jumps over the lazy dog.") == false) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "Test with input sentence \"The quick brown fox jumps over the lazy dog.\".", score: pts};
+// }
+
+// function run_tests(fn) {
+//     var tests = [test0, test1, test2, test3, test4];
+//     var results = [];
+
+//     for (var i = 0; i < tests.length; i++) {
+//         results.push(tests[i](fn));
+//     }
+
+//     return results;
+// }
 
 module.exports = { run_tests };
--- server/unit_tests/q7_tests.js
+++ server/unit_tests/q7_tests.js
@@ -1,39 +1,92 @@
-function test0(fn) {
-    var pts = 0;
-
-    if (fn(0) === 1 && fn(1) === 1) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "Test base cases.", score: pts};
-}
-
-function test1(fn) {
-    var pts = 0;
-
-    if (fn(7) === 5040) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "A less basic test to check with moderate size output.", score: pts};
-}
-
-function test2(fn) {
-    var pts = 0;
-
-    if (fn(18) === 6402373705728000) pts = 1;
-    else pts = 0;
-    
-    return {"desc": "An input that's meant to generate a massive output.", score: pts};
+var tests = {
+    "test1": {
+        "input_args": [[0], [1]],
+        "expected_outputs": [1, 1],
+        "pts": 1,
+        "desc": "Test base cases.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test2": {
+        "input_args": [[7]],
+        "expected_outputs": [5040],
+        "pts": 1,
+        "desc": "A less basic test to check with moderate size output.",
+        "passed": false,
+        "actual_outputs": []
+    },
+    "test3": {
+        "input_args": [[18]],
+        "expected_outputs": [6402373705728000],
+        "pts": 1,
+        "desc": "An input that's meant to generate a massive output.",
+        "passed": false,
+        "actual_outputs": []
+    }
 }
 
 function run_tests(fn) {
-    var tests = [test0, test1, test2];
-    var results = [];
+    var results = []
+    const keys = Object.keys(tests);
+    for (var i = 0; i < keys.length; i++) {
+        const curTest = tests[keys[i]]
+        const args = curTest.input_args;
+        const outputs = curTest.expected_outputs;
+        results.push(curTest);
+        for (var j = 0; j < curTest.input_args.length; j++) {
+            try {
+                const actualOutput = fn(...args[j])
+                const expectedOutput = outputs[j]
 
-    for (var i = 0; i < tests.length; i++) {
-        results.push(tests[i](fn));
+                if (actualOutput == expectedOutput) {
+                    results[i].passed = true;
+                    results[i].actual_outputs.push(actualOutput);
+                }
+            } catch (err) {
+                results[i].actual_outputs.push(err);
+            }
+        }
     }
 
     return results;
 }
+
+// function test0(fn) {
+//     var pts = 0;
+
+//     if (fn(0) === 1 && fn(1) === 1) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "Test base cases.", score: pts};
+// }
+
+// function test1(fn) {
+//     var pts = 0;
+
+//     if (fn(7) === 5040) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "A less basic test to check with moderate size output.", score: pts};
+// }
+
+// function test2(fn) {
+//     var pts = 0;
+
+//     if (fn(18) === 6402373705728000) pts = 1;
+//     else pts = 0;
+    
+//     return {"desc": "An input that's meant to generate a massive output.", score: pts};
+// }
+
+// function run_tests(fn) {
+//     var tests = [test0, test1, test2];
+//     var results = [];
+
+//     for (var i = 0; i < tests.length; i++) {
+//         results.push(tests[i](fn));
+//     }
+
+//     return results;
+// }
 
 module.exports = { run_tests };
